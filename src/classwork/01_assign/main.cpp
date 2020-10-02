c5dcfdf002aa6bf74ccbf9a744c6cc01a47ceda5
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-10-02 11:37:37 -0500

    hw0 grading

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,5 +1,6 @@
 //Write the include statement for types.h here
 #include<iostream>
+#include "types.h"
 
 //Write include for capturing input from keyboard and displaying output to screen
 
@@ -11,17 +12,12 @@ int main()
 	cout<<"Enter a number: ";//Text to display to screen.
 	cin>>num;//Capture a number from the keyboard.
 	cout<<"You entered: "<<num; 
-	int result;
-	int multiply_numbers(num)
-	{
-		return>>result;
-	}
+	int result = multiply_numbers(num);
+	
 	cout<<"Your number times 5 is: "<<result;
 	int num1 = 4;
-	int multiply_numbers(num1)
-	{
-		return>>result;
-	}
+	result = multiply_numbers(num1);
+	
 	cout<<"4 times 5 is: "<<result;
 	
 	
